fa75444878603492fb54a4563018aea66b626953
qnnp <58357109+qnnp-me@users.noreply.github.com>
2025-03-13 00:11:43 +0800

    refactor(wegar-remote-component): 将 RemoteComponent 的参数改为接口形式
    - 新增 RemoteComponentProps 接口，用于定义 RemoteComponent 的参数
    - 使代码结构更加清晰，提高可维护性和可读性
    - 不改变原有功能，仅对代码结构进行优化

--- src/RemoteComponent.tsx
+++ src/RemoteComponent.tsx
@@ -1,6 +1,49 @@
 import {createElement, FunctionComponent, ReactNode, useEffect, useState} from "react";
 
-const RemoteComponent = <T extends Record<string, unknown>>(
+export interface RemoteComponentProps<T extends Record<string, unknown>> {
+  /**
+   * 组件文件地址
+   */
+  url: string
+  /**
+   * 如果 css 为 true, 则使用 url 最后一个 / 后面的文件名作为 css 文件名，否则视为直接 css 文件地址
+   *
+   * ```
+   * 如 /js/index.umd.js 则加载 /css/index.css
+   * ```
+   */
+  css?: boolean | string
+  /**
+   * 如果 name 为空, 则使用 url 最后一个 / 后面的文件名作为组件名
+   *
+   * ```
+   * 如 /js/index.umd.js 则 name 为 index
+   * ```
+   */
+  name?: string
+  /**
+   * 组件的 props
+   */
+  props?: T
+  /**
+   * 组件加载中的 loading 组件
+   */
+  loading?: FunctionComponent
+  /**
+   * 如果没有 loading 组件, 则使用 children 作为组件加载中的 loading 组件
+   */
+  children?: ReactNode
+  /**
+   * 组件加载失败的 fallback 组件
+   */
+  fallback?: FunctionComponent
+  /**
+   * 是否已挂载的缓存组件
+   */
+  cache?: boolean
+}
+
+function RemoteComponent<T extends Record<string, unknown>>(
   {
     url,
     css = false,
@@ -10,49 +53,7 @@ const RemoteComponent = <T extends Record<string, unknown>>(
     loading,
     fallback,
     cache = true
-  }: {
-    /**
-     * 组件文件地址
-     */
-    url: string
-    /**
-     * 如果 css 为 true, 则使用 url 最后一个 / 后面的文件名作为 css 文件名，否则视为直接 css 文件地址
-     *
-     * ```
-     * 如 /js/index.umd.js 则加载 /css/index.css
-     * ```
-     */
-    css?: boolean | string
-    /**
-     * 如果 name 为空, 则使用 url 最后一个 / 后面的文件名作为组件名
-     *
-     * ```
-     * 如 /js/index.umd.js 则 name 为 index
-     * ```
-     */
-    name?: string
-    /**
-     * 组件的 props
-     */
-    props?: T
-    /**
-     * 组件加载中的 loading 组件
-     */
-    loading?: FunctionComponent
-    /**
-     * 如果没有 loading 组件, 则使用 children 作为组件加载中的 loading 组件
-     */
-    children?: ReactNode
-    /**
-     * 组件加载失败的 fallback 组件
-     */
-    fallback?: FunctionComponent
-    /**
-     * 是否已挂载的缓存组件
-     */
-    cache?: boolean
-  }
-): ReactNode => {
+  }: RemoteComponentProps<T>): ReactNode {
   const scriptFileName = url.replace(/.*?([^/.]+)[.\w]+$/, '$1');
   const [componentLoading, setComponentLoading] = useState(!window[(name || scriptFileName) as never])
 
@@ -93,4 +94,5 @@ const RemoteComponent = <T extends Record<string, unknown>>(
   }
   return fallback && createElement(fallback)
 }
+
 export default RemoteComponent
